d5e749c209f31659240c26a442100a2530a125a1
Jack Ewing <je87966@eanesisd.net>
2023-11-23 00:14:34 -0800

    april tag code

--- GUI/app/tabs/vmc_control.py
+++ GUI/app/tabs/vmc_control.py
@@ -1,26 +1,73 @@
 from __future__ import annotations
+#from pynput import keyboard
 
 import functools
+import json
+import base64
+import time
+import time
+
 from typing import List, Literal, Tuple
 
 from bell.avr.mqtt.payloads import (
     AvrPcmSetBaseColorPayload,
+    AvrPcmSetServoAbsPayload,
     AvrPcmSetServoOpenClosePayload,
+    AvrApriltagsVisiblePayload,
+    #AvrAprilTagsFpsPayload
 )
 from PySide6 import QtCore, QtWidgets
+from numpy import cumprod
 
 from ..lib.color import wrap_text
 from .base import BaseTabWidget
 
-
 class VMCControlWidget(BaseTabWidget):
-    # This is the primary control widget for the drone. This allows the user
-    # to set LED color, open/close servos etc.
 
     def __init__(self, parent: QtWidgets.QWidget) -> None:
         super().__init__(parent)
-
         self.setWindowTitle("VMC Control")
+
+        #drop distance defintions (mm)
+        self.nearZone = 40
+        self.dropZone = 20
+
+        #time interval between actions
+        self.timeInterval = 1
+
+        #led strip colors
+        self.colorPurple = (10, 160, 32, 240)
+        self.colorRed = (255, 255, 0, 0)
+        self.colorBlue = (255, 0, 0, 255)
+        self.colorGreen = (255, 0, 255, 0)
+        self.colorYellow = (255, 255, 255, 0)
+        self.colorBlank = (0,0,0,0)
+
+        #servo parameters
+        self.servoPin = 7
+        self.openPulse = 800
+        self.closePulse = 2000
+
+        #STATIC state variables
+        self.UNARMED = 0
+        self.ARMED = 1
+        self.DROPPING = 2
+
+        self.OPENING = 3
+        self.CLOSING = 4
+
+        self.augurPosition = 0;
+
+        #time of the last drop
+        self.lastDrop = 0
+
+        self.actionCount = 0
+        #number of drops wanted
+        self.targetDropAction = 3
+
+        #current state
+        self.state = self.ARMED
+
 
     def build(self) -> None:
         """
@@ -29,92 +76,62 @@ class VMCControlWidget(BaseTabWidget):
         layout = QtWidgets.QGridLayout(self)
         self.setLayout(layout)
 
-        # ==========================
-        # LEDs
-        led_groupbox = QtWidgets.QGroupBox("LEDs")
-        led_layout = QtWidgets.QVBoxLayout()
-        led_groupbox.setLayout(led_layout)
+    # Function to move the auger
+    def moveAugur(self) -> None:
+        #if drop complete, move to unarmed
+        if(self.actionCount < 0):
+            self.state = self.ARMED
+            return
+        #of drop in progress, toggle the augur
+        self.toggleAugur()
 
-        red_led_button = QtWidgets.QPushButton("Red")
-        red_led_button.setStyleSheet("background-color: red")
-        red_led_button.clicked.connect(lambda: self.set_led((255, 255, 0, 0)))  # type: ignore
-        led_layout.addWidget(red_led_button)
+        #updates time
+        self.lastDrop = time.time()
+        #updates drop count
+        print(self.actionCount)
+        self.actionCount -= 1
 
-        green_led_button = QtWidgets.QPushButton("Green")
-        green_led_button.setStyleSheet("background-color: green")
-        green_led_button.clicked.connect(lambda: self.set_led((255, 0, 255, 0)))  # type: ignore
-        led_layout.addWidget(green_led_button)
+    #toggleAugur - will open or seal the augur based on the prior state
+    def toggleAugur(self)->None:
+        if(self.augurPosition != self.OPENING):
+            self.augurPosition = self.OPENING
+            self.drop()
+        else:
+            self.augurPosition = self.CLOSING
+            self.seal()
 
-        blue_led_button = QtWidgets.QPushButton("Blue")
-        blue_led_button.setStyleSheet("background-color: blue; color: white")
-        blue_led_button.clicked.connect(lambda: self.set_led((255, 0, 0, 255)))  # type: ignore
-        led_layout.addWidget(blue_led_button)
+    #seal - command the serve to close the augur to sealed position
+    def seal(self)->None:
+        self.set_servo_pos( self.servoPin, self.closePulse)
+        self.set_led(self.colorBlue)
 
-        clear_led_button = QtWidgets.QPushButton("Clear")
-        clear_led_button.setStyleSheet("background-color: white")
-        clear_led_button.clicked.connect(lambda: self.set_led((0, 0, 0, 0)))  # type: ignore
-        led_layout.addWidget(clear_led_button)
+    #drop - command the servo to open the augur
+    def drop(self)-> None:
+        self.set_servo_pos(self.servoPin, self.openPulse)
+        self.set_led(self.colorRed)
 
-        layout.addWidget(led_groupbox, 0, 0, 3, 1)
 
-        # ==========================
-        # Servos
-        self.number_of_servos = 4
-        self.servo_labels: List[QtWidgets.QLabel] = []
+    def process_message(self, topic: str, payload: str) -> None:
+        """
+        Process an incoming message and update the appropriate component
+        """
+        if(self.state == self.DROPPING and time.time() > (self.lastDrop + self.timeInterval)):
+            self.moveAugur()
 
-        servos_groupbox = QtWidgets.QGroupBox("Servos")
-        servos_layout = QtWidgets.QVBoxLayout()
-        servos_groupbox.setLayout(servos_layout)
+        # discard topics we don't recognize
+        elif topic == "avr/apriltags/visible":
+            print("read topic")
 
-        servo_all_layout = QtWidgets.QHBoxLayout()
+            x_json = json.loads(payload)
+            horizontal_dist = x_json["tags"][0]["horizontal_dist"]
 
-        servo_all_open_button = QtWidgets.QPushButton("Open all")
-        servo_all_open_button.clicked.connect(lambda: self.set_servo_all("open"))  # type: ignore
-        servo_all_layout.addWidget(servo_all_open_button)
+            if(horizontal_dist < self.nearZone and horizontal_dist > self.dropZone):
+                self.set_led(self.colorYellow)
+            elif(horizontal_dist <= self.dropZone and self.state == self.ARMED):
+                self.set_led(self.colorGreen)
+                self.state = self.DROPPING
+                self.actionCount = self.targetDropAction * 2
 
-        servo_all_close_button = QtWidgets.QPushButton("Close all")
-        servo_all_close_button.clicked.connect(lambda: self.set_servo_all("close"))  # type: ignore
-        servo_all_layout.addWidget(servo_all_close_button)
-
-        servos_layout.addLayout(servo_all_layout)
-
-        for i in range(self.number_of_servos):
-            servo_groupbox = QtWidgets.QGroupBox(f"Servo {i+1}")
-            servo_layout = QtWidgets.QHBoxLayout()
-            servo_groupbox.setLayout(servo_layout)
-
-            servo_open_button = QtWidgets.QPushButton("Open")
-            servo_open_button.clicked.connect(functools.partial(self.set_servo, i, "open"))  # type: ignore
-            servo_layout.addWidget(servo_open_button)
-
-            servo_close_button = QtWidgets.QPushButton("Close")
-            servo_close_button.clicked.connect(functools.partial(self.set_servo, i, "close"))  # type: ignore
-            servo_layout.addWidget(servo_close_button)
-
-            servo_label = QtWidgets.QLabel()
-            servo_label.setAlignment(
-                QtCore.Qt.AlignmentFlag.AlignRight
-                | QtCore.Qt.AlignmentFlag.AlignVCenter
-            )
-            servo_layout.addWidget(servo_label)
-            self.servo_labels.append(servo_label)
-
-            servos_layout.addWidget(servo_groupbox)
-
-        layout.addWidget(servos_groupbox, 0, 1, 3, 3)
-
-        # # ==========================
-        # # PCC Reset
-        # reset_groupbox = QtWidgets.QGroupBox("Reset")
-        # reset_layout = QtWidgets.QVBoxLayout()
-        # reset_groupbox.setLayout(reset_layout)
-
-        # reset_button = QtWidgets.QPushButton("Reset PCC")
-        # reset_button.setStyleSheet("background-color: yellow")
-        # reset_button.clicked.connect(lambda: self.send_message("avr/pcm/reset", AvrPcmResetPayload()))  # type: ignore
-        # reset_layout.addWidget(reset_button)
-
-        # layout.addWidget(reset_groupbox, 3, 3, 1, 1)
 
     def set_servo(self, number: int, action: Literal["open", "close"]) -> None:
         """
@@ -125,26 +142,20 @@ class VMCControlWidget(BaseTabWidget):
             AvrPcmSetServoOpenClosePayload(servo=number, action=action),
         )
 
-        if action == "open":
-            text = "Opened"
-            color = "blue"
-        else:
-            text = "Closed"
-            color = "chocolate"
-
-        self.servo_labels[number].setText(wrap_text(text, color))
-
-    def set_servo_all(self, action: Literal["open", "close"]) -> None:
-        """
-        Set all servos to the same state
-        """
-        for i in range(self.number_of_servos):
-            self.set_servo(i, action)
-
     def set_led(self, color: Tuple[int, int, int, int]) -> None:
         """
         Set LED color
         """
         self.send_message(
             "avr/pcm/set_base_color", AvrPcmSetBaseColorPayload(wrgb=color)
+        )
+
+    def set_servo_pos(self, number: int, position: int) -> None:
+        """
+        Set a servo state
+        """
+
+        self.send_message(
+            "avr/pcm/set_servo_abs",
+            AvrPcmSetServoAbsPayload(servo=number, absolute=position),
         )
